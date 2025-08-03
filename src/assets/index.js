a9366f9bcd2865b52b299aa838f7ea62fa77f353
chaithanya37 <chaithanyakanipakam37@gmail.com>
2025-08-03 18:51:57 +0530

    assets are added

---
+++ src/assets/index.js
@@ -0,0 +1,79 @@
+
+import javadev from "./javadev.png";
+import frontend from "./Frontend.png";
+import design from "./Design.png";
+import backend from "./Backend.png";
+import fullStack from "./FullStack.png";
+
+import menu from "./Menu.svg";
+import close from "./Close.svg";
+
+import html from "./tech/Html.png";
+import css from "./tech/Css.png";
+import javascript from "./tech/Javascript.png";
+import reactjs from "./tech/Reactjs.png";
+import redux from "./tech/Redux.png";
+import tailwind from "./tech/Tailwind.png";
+import nodejs from "./tech/Nodejs.png";
+import expressjs from "./tech/Expressjs.png";
+import mongodb from "./tech/Mongodb.png";
+import mongoose from "./tech/Mongoose.png";
+import git from "./tech/Git.png";
+import github from "./Github.png";
+
+import vscode from "./tools/Vscode.png";
+import ChatGPT from "./tools/Chatgpt.png";
+import chrome from "./tools/Chrome.png";
+import mongodbCompass from "./tools/mongodbcompass.png";
+import postman from "./tools/Postman.png";
+import ubuntu from "./tools/Ubuntu.png";
+import ECommerce from "./ECommerce.png";
+import Handyman from "./Handyman.png";
+import ChatApp from "./ChatApp.png";
+import java from "./tech/Java.png";
+import python from "./tech/python.png";
+import c from "./tech/c.png";
+import bootstrap from "./tech/bootstrap.png";
+import mysql from "./tech/mysql.png";
+import postgres from "./tech/Postgres.png";
+import live from "./live.png";
+
+
+export {
+  menu,
+  close,
+  javadev,
+  frontend,
+  design,
+  backend,
+  fullStack,
+  html,
+  css,
+  javascript,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  expressjs,
+  mongodb,
+  mongoose,
+  git,
+  github,
+  vscode,
+  ChatGPT,
+  chrome,
+  mongodbCompass,
+  postman,
+  ubuntu,
+  ECommerce,
+  Handyman,
+  ChatApp,
+  java,
+  python,
+  c,
+  bootstrap,
+  postgres,
+  mysql,
+  live
+
+};
---
+++ src/assets/svg/index.js
@@ -0,0 +1,5 @@
+
+import { ReactComponent as ReactLogo } from "./";
+
+export { ReactLogo };
+{/* <Close width="32" height="32" /> */}
